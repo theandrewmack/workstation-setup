07ebdca78ee4229b029c03e4c55c767d805e4448
Andrew Mack <theandrewmack@gmail.com>
2024-11-10 20:23:08 -0600

    remove analytics

--- setup.sh
+++ setup.sh
@@ -14,17 +14,10 @@ set -e
 
 echo "Caching password..."
 sudo -K
-sudo true;
+sudo true
 clear
 
 MY_DIR="$(dirname "$0")"
-SKIP_ANALYTICS=${SKIP_ANALYTICS:-0}
-if (( SKIP_ANALYTICS == 0 )); then
-    clientID=$(od -vAn -N4 -tx  < /dev/urandom)
-    source ${MY_DIR}/scripts/helpers/google-analytics.sh ${clientID} start $@
-else
-    export HOMEBREW_NO_ANALYTICS=1
-fi
 
 # Note: Homebrew needs to be set up first
 source ${MY_DIR}/scripts/common/homebrew.sh
@@ -40,19 +33,15 @@ source ${MY_DIR}/scripts/common/unix.sh
 source ${MY_DIR}/scripts/common/configuration-osx.sh
 
 # For each command line argument, try executing the corresponding script in opt-in/
-for var in "$@"
-do
+for var in "$@"; do
     echo "$var"
     FILE=${MY_DIR}/scripts/opt-in/${var}.sh
     echo "$FILE"
     if [ -f $FILE ]; then
         source ${FILE}
     else
-       echo "Warning: $var does not appear to be a valid argument. File $FILE does not exist."
+        echo "Warning: $var does not appear to be a valid argument. File $FILE does not exist."
     fi
 done
 
 source ${MY_DIR}/scripts/common/finished.sh
-if (( SKIP_ANALYTICS == 0 )); then
-    source ${MY_DIR}/scripts/helpers/google-analytics.sh ${clientID} finish $@
-fi
